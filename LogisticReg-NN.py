73dba45055cc91e0f3e2c51f52e8ba53a50afade
Ahmed Faraz <47153672+Ahmiii@users.noreply.github.com>
2019-01-29 23:41:21 +0500

    Update LogisticReg-NN.py

--- LogisticReg-NN.py
+++ LogisticReg-NN.py
@@ -53,24 +53,7 @@ def sigmoid(z):
 	return s
 
 def propagate(w, b, X, Y):
-    """
-    Implement the cost function and its gradient for the propagation explained above
-
-    Arguments:
-    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
-    b -- bias, a scalar
-    X -- data of size (num_px * num_px * 3, number of examples)
-    Y -- true "label" vector (containing 0 if non-cat, 1 if cat) of size (1, number of examples)
-
-    Return:
-    cost -- negative log-likelihood cost for logistic regression
-    dw -- gradient of the loss with respect to w, thus same shape as w
-    db -- gradient of the loss with respect to b, thus same shape as b
-    
-    Tips:
-    - Write your code step by step for the propagation. np.log(), np.dot()
-    """
-    
+        
     m = X.shape[1]
     
     # FORWARD PROPAGATION (FROM X TO COST)
@@ -103,28 +86,7 @@ def propagate(w, b, X, Y):
 
 
 def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
-    """
-    This function optimizes w and b by running a gradient descent algorithm
-    
-    Arguments:
-    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
-    b -- bias, a scalar
-    X -- data of shape (num_px * num_px * 3, number of examples)
-    Y -- true "label" vector (containing 0 if non-cat, 1 if cat), of shape (1, number of examples)
-    num_iterations -- number of iterations of the optimization loop
-    learning_rate -- learning rate of the gradient descent update rule
-    print_cost -- True to print the loss every 100 steps
-    
-    Returns:
-    params -- dictionary containing the weights w and bias b
-    grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
-    costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
-    
-    Tips:
-    You basically need to write down two steps and iterate through them:
-        1) Calculate the cost and the gradient for the current parameters. Use propagate().
-        2) Update the parameters using gradient descent rule for w and b.
-    """
+  
     
     costs = []
     
@@ -166,17 +128,7 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
 
 
 def predict(w, b, X):
-    '''
-    Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
-    
-    Arguments:
-    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
-    b -- bias, a scalar
-    X -- data of size (num_px * num_px * 3, number of examples)
-    
-    Returns:
-    Y_prediction -- a numpy array (vector) containing all predictions (0/1) for the examples in X
-    '''
+   
     
     m = X.shape[1]
     Y_prediction = np.zeros((1,m))
@@ -205,21 +157,7 @@ def predict(w, b, X):
     return Y_prediction
 
 def model(X_train, Y_train, num_iterations = 2000, learning_rate = 0.5, print_cost = False):
-    """
-    Builds the logistic regression model by calling the function you've implemented previously
     
-    Arguments:
-    X_train -- training set represented by a numpy array of shape (num_px * num_px * 3, m_train)
-    Y_train -- training labels represented by a numpy array (vector) of shape (1, m_train)
-    X_test -- test set represented by a numpy array of shape (num_px * num_px * 3, m_test)
-    Y_test -- test labels represented by a numpy array (vector) of shape (1, m_test)
-    num_iterations -- hyperparameter representing the number of iterations to optimize the parameters
-    learning_rate -- hyperparameter representing the learning rate used in the update rule of optimize()
-    print_cost -- Set to true to print the cost every 100 iterations
-    
-    Returns:
-    d -- dictionary containing information about the model.
-    """
     
     ### START CODE HERE ###
     
